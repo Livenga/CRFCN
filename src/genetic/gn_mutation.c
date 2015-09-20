f53c28c0a7827f9cb4a50ed4dbf77c0756939be0
Livenga <ljmmz0405@gmail.com>
2015-09-20 09:46:31 +0000

    ユニット数の突然変異の追加

--- src/genetic/gn_mutation.c
+++ src/genetic/gn_mutation.c
@@ -16,7 +16,7 @@ void gmutation(genotype gtype) {
   switch(point_y) {
     case 1: value = STEP;       break;
     case 2: value = alpha_size; break;
-    case 3: value = 1;          break;
+    case 3: value = 2;          break;
     case 4: value = thr_size;   break;
     default: value = w_size;    break;
   }
@@ -27,4 +27,38 @@ void gmutation(genotype gtype) {
 }
 
 void gunit_mutation(genotype gtype) {
+  int i, j, add, value;
+  const int column = (gtype[0] >> 8) & 0xFF, row = (gtype[0] & 0xFF);
+  extern int alpha_size, thr_size, w_size;
+
+  add = ((column + 2) >= MAX_UNIT_SIZE) ? -1 : (((column + 2) <= 5) ? 1 :
+      (rand() % 2) == 0) ? 1 : -1;
+
+  if(add == 1) { // 増加
+    for(i = 1; i < row; i++) {
+      switch(i) {
+        case 1 : value = STEP;       break;
+        case 2 : value = alpha_size; break;
+        case 3 : value = 2;          break;
+        case 4 : value = thr_size;   break;
+        default: value = w_size;     break;
+      }
+      gtype[i] = (gtype[i] << 4) + (rand() % value);
+    }
+
+    gtype[row] = 0x00;
+    int shift_size;
+    for(i = 0; i < column + 3; i++) {
+      shift_size = (column + 2 - i) * 4;
+      gtype[row] += (rand() % w_size) << shift_size;
+    }
+    gtype[0] += 0x000101;
+  }
+
+  else if(add == -1) { // 減少
+    for(i = 1; i < row + 1; i++)
+      gtype[i] = gtype[i] >> 4;
+    gtype[row + 1] = 0x00;
+    gtype[0] -= 0x000101;
+  }
 }
